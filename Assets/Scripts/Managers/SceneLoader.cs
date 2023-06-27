e74918f18a179feadf767d1d2a31743fcb5e9277
bcanevaro <bcanevaro@itba.edu.ar>
2023-06-27 19:04:43 -0300

    Added Loading Screen

---
+++ Assets/Scripts/Managers/SceneLoader.cs
@@ -0,0 +1,26 @@
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider loadingBar;
+    public void LoadScene(int levelIndex)
+    {
+        StartCoroutine(LoadSceneAsynchronously(levelIndex));
+    }
+
+    IEnumerator LoadSceneAsynchronously(int levelIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(levelIndex);
+        loadingScreen.SetActive(true);
+        while (!operation.isDone)
+        {
+            loadingBar.value = operation.progress;
+            yield return null;
+        }
+    }
+}
